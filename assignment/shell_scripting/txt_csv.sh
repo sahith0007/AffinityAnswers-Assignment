c1369032affe1337fa14d83195145c15d9c99164
sahith_07 <93025693+sahith0007@users.noreply.github.com>
2023-03-19 17:30:20 +0530

    Update txt_csv.sh

--- assignment/shell_scripting/txt_csv.sh
+++ assignment/shell_scripting/txt_csv.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
+#Downloading the required txt file from the url
 wget -O NAVAll.txt http://www.amfiindia.com/spages/NAVAll.txt
+
+#Extracting the required Data Fields
 cat NAVAll.txt | cut -d ';' -f 4,5 > shell_output.csv
+
+#Removing the .txt file 
 rm NAVAll.txt
 echo "Done. Results saved in shell_output.csv"
